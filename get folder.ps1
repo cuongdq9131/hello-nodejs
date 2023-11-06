921bea5934311a0f39c17b69e3c2ab079a941753
cuongdq9131 <120432939+cuongdq9131@users.noreply.github.com>
2023-11-06 10:50:31 +0700

    Add files via upload

---
+++ get folder.ps1
@@ -0,0 +1,15 @@
+﻿$targetfolder='H:\Setup'
+$dataColl = @()
+gci -recurse -force $targetfolder -ErrorAction SilentlyContinue | ? { $_ -is [io.directoryinfo] } | foreach {
+$len = 0
+gci -recurse -force $_.fullname -ErrorAction SilentlyContinue | % { $len += $_.length }
+$foldername = $_.fullname
+$foldersize= '{0:N2}' -f ($len / 1Mb)
+$dataObject = New-Object PSObject
+Add-Member -inputObject $dataObject -memberType NoteProperty -name “Folder Name” -value $foldername
+Add-Member -inputObject $dataObject -memberType NoteProperty -name “Folder SizeGb” -value $foldersize
+$dataColl += $dataObject
+}
+
+$dataColl|Sort-object @{Expression = "Folder Name";}| Out-GridView -Title “Size of subdirectories”
+#$dataColl|Export-Csv "F:\Setup\abc.csv"
